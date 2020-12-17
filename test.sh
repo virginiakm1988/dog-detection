01f0d4e2100217aa1189a4b5f4a8371eddf4152b
Eric Xu <avatar.xu@gmail.com>
2020-12-17 18:37:49 +0800

    Mfshiu join

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-python3 inference.py $1 $2 $3 $4 
+python3 inference.py $1 $2 $3 $4
+# bash test.sh ./農委會狗鼻_手標/0001/0.PNG ./農委會狗鼻_手標/0001/1.PNG 0.5 model.pkl
+# bash test-gpu.sh ./農委會狗鼻_手標/0001/0.PNG ./農委會狗鼻_手標/0001/1.PNG 0.5 model.pkl
 # bash test.sh ./data/0.PNG ./data/1.PNG 0.5 ./model/Siamese_model.pkl
